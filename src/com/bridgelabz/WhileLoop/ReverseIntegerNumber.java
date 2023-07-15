b7db77dc506bddeddbe405fe0dd0d4a27ae58834
drisyaathul <drisyak809@gmail.com>
2023-07-15 19:22:07 +0800

    3.Reverse the Integer Number

---
+++ src/com/bridgelabz/WhileLoop/ReverseIntegerNumber.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.WhileLoop;
+//Write a Program to reverse the integer number eg. Input n=231 reverse is 132
+import java.util.Scanner;
+
+public class ReverseIntegerNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Integer Number : ");
+        int number = scanner.nextInt();
+
+        int reverse = 0;
+        int i = 0;
+
+        while (number>=1) {
+            i = number%10;
+            reverse = reverse*10 + i;
+            number = number/10;
+        }
+        System.out.println("Reverse the Number : "+reverse);
+    }
+}
